0ba692ec7922e2b7e39f244d503c07daa1bacd69
Jelena <jsmiljkovic94@gmail.com>
2026-09-22 19:17:07 +0200

    Update 0001_initial_schema.py

--- alembic/versions/0001_initial_schema.py
+++ alembic/versions/0001_initial_schema.py
@@ -8,8 +8,9 @@ Create Date: 2026-09-22
 from collections.abc import Sequence
 
 import sqlalchemy as sa
-from alembic import op
 from sqlalchemy.dialects import postgresql
+
+from alembic import op
 
 revision: str = "0001"
 down_revision: str | None = None
