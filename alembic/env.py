aeaec6e9666d379af5ea80e4ad0ff0056c5a12a5
Jelena <jsmiljkovic94@gmail.com>
2026-09-22 18:56:11 +0200

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -1,16 +1,13 @@
 import asyncio
 from logging.config import fileConfig
 
+import app.models  # noqa: F401
 from alembic import context
 from sqlalchemy import pool
-from sqlalchemy.ext.asyncio import (
-    async_engine_from_config,
-)
+from sqlalchemy.ext.asyncio import async_engine_from_config
 
-import app.models  # noqa: F401
 from app.core.config import get_settings
 from app.db.base import Base
-
 
 config = context.config
 
