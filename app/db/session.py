1dee4e2db1f1f1bf3e46a58d0f73af65cba13836
Jelena <jsmiljkovic94@gmail.com>
2026-09-22 18:58:26 +0200

    Update session.py

--- app/db/session.py
+++ app/db/session.py
@@ -1,13 +1,8 @@
 from collections.abc import AsyncGenerator
 
-from sqlalchemy.ext.asyncio import (
-    AsyncSession,
-    async_sessionmaker,
-    create_async_engine,
-)
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from app.core.config import get_settings
-
 
 settings = get_settings()
 
@@ -23,6 +18,9 @@ AsyncSessionFactory = async_sessionmaker(
 )
 
 
-async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
+async def get_db_session() -> AsyncGenerator[
+    AsyncSession,
+    None,
+]:
     async with AsyncSessionFactory() as session:
         yield session
