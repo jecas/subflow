b03b0a3a6d9091ca181c855553233d504f7c618d
Jelena <jsmiljkovic94@gmail.com>
2026-09-22 16:00:06 +0200

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,12 @@
+from app.models.customer import Customer, CustomerRole
+from app.models.plan import BillingPeriod, Plan
+from app.models.subscription import Subscription, SubscriptionStatus
 
+__all__ = [
+    "Customer",
+    "CustomerRole",
+    "Plan",
+    "BillingPeriod",
+    "Subscription",
+    "SubscriptionStatus",
+]
