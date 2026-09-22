b1f3dde1492a59a57b55bfe429f003008b33bb61
Jelena <jsmiljkovic94@gmail.com>
2026-09-22 20:20:21 +0200

    Add files via upload

---
+++ app/core/middleware.py
@@ -0,0 +1,28 @@
+import logging
+from time import perf_counter
+from uuid import uuid4
+
+from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware
+
+logger = logging.getLogger("subflow.request")
+
+
+class RequestLoggingMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        request_id = request.headers.get("X-Request-ID", uuid4().hex)
+        started = perf_counter()
+        response = await call_next(request)
+        duration_ms = round((perf_counter() - started) * 1000, 2)
+        response.headers["X-Request-ID"] = request_id
+        logger.info(
+            "request_completed",
+            extra={
+                "request_id": request_id,
+                "method": request.method,
+                "path": request.url.path,
+                "status_code": response.status_code,
+                "duration_ms": duration_ms,
+            },
+        )
+        return response
